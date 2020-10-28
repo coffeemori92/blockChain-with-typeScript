7730ec541200612fcc807785f05b086092bb1f2c
coffeemori92 <coffeemori92@naver.com>
2020-10-28 19:01:33 +0900

    ts class

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
-interface Human {
-  name: string,
-  age: number,
-  gender: string,
-};
+class Human {
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+  public name: string;
+  public age: number;
+  public gender: string;
+}
 
-const person = {
-  name: 'coffeemori',
-  gender: 'male',
-  age: 24,
-};
+const human = new Human('coffeemori', 18, 'male');
 
 const sayHi = (person: Human): void => {
   console.log(`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`);
 };
 
-sayHi(person);
+sayHi(human);
