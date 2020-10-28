8a91fac472ceb1304f6a0ef36503c63a26d4cdaf
coffeemori92 <coffeemori92@naver.com>
2020-10-28 18:32:40 +0900

    타입스크립트에서 함수에 매개변수 사용하기

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log('Hello TS');
+const person = 'coffeemori';
+const age = 24;
+const gender = 'male';
+
+const sayHi = (name, age, gender?) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`)
+};
+
+sayHi(person, age);
